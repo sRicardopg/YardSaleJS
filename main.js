3beb6f3f65398536b854be580df3cd2bf2648737
sRicardopg <samuelricardopg5@gmail.com>
2023-04-23 11:57:04 -0500

    Now burger menu list was integrated, fixed a pair of responsive issues

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
-menuEmail.addEventListener("click", toggleElement)
-function toggleElement(){
+const burgerMenu = document.querySelector(".menu")
+const burgerList = document.querySelector(".mobile-menu")
+function toggleDeskMenu(){
     desktopMenu.classList.toggle("inactive")
 }
+function toggleBurgerMenu(){
+    burgerList.classList.toggle("inactive")
+}
+menuEmail.addEventListener("click", toggleDeskMenu)
+
+burgerMenu.addEventListener("click", toggleBurgerMenu)
